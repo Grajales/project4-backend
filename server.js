e5652647c90b802526ad95e26741c1f9f82e9ff8
Bingcheng Ni <nibingcheng@johndeere.com>
2021-03-11 17:45:21 -0600

    Added artistsController

--- server.js
+++ server.js
@@ -30,11 +30,13 @@ app.use(methodOverride("_method"));
 
 // HOMEPAGE
 app.get("/", (req, res) => {
-  res.json({ message: "express api app is working" });
+  res.json({ message: "express api app is working", 
+             array:["Paula", "seth", "Mark"] });           
 });
 
 app.use("/api/auth", require("./controllers/authController.js"));
 app.use("/api/users", require("./controllers/usersController.js"));
+app.use("/api/artists", require("./controllers/artistsController.js"));
 
 app.listen(process.env.PORT, () => {
   console.log("Nodemon listening");
